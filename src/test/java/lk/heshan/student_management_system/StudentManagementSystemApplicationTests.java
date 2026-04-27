5260fbbacddbf4960ea119e0ffd669d5825a47cd
heshan <heshansandakelum2018@gmail.com>
2026-04-27 16:00:08 +0530

    add dependencies

---
+++ src/test/java/lk/heshan/student_management_system/StudentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.heshan.student_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
